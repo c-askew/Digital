612f4a33bb853b3d76dffe9774834edfad443c05
caskew2154 <caskew2154@gmail.com>
2018-03-11 01:14:39 +0000

    Search v2.0
    Updated to use The Movie Database API

--- scripts/search.js
+++ scripts/search.js
@@ -4,7 +4,7 @@ var imageURL = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
 var backdropURL = "https://image.tmdb.org/t/p/w1400_and_h450_face/";
 var genreAPI = "https://api.themoviedb.org/3/genre/tv/list?api_key=" + apiKey + "&language=en-GB";
 var tvDetails = "https://api.themoviedb.org/3/" +  + "tv/list?api_key=" + apiKey + "&language=en-GB";
-var fullDetails = {};
+
 
 
 function parseQuery() {
@@ -15,8 +15,8 @@ function parseQuery() {
   fetchData(sendQuery);
 };
 
+//Get's initial search results
 function fetchData (sendQuery) {
-
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -30,41 +30,53 @@ function fetchData (sendQuery) {
     http.send();
 };
 
-function displayData(data) {
-
-  var results = data.results;
+function displayData(mediaData) {
+  var results = mediaData.results;
   console.log(results)
   var resultsContainer = document.getElementById('results-container');
+  var fullDetails = {};
+
 
   if (results.length > 10 ? count = 10 : count = results.length);
   for (var i = 0; i < count; i++ ) {
     //Collecting Values
     var curResult = 'result' + i;
-    if (results[i].hasOwnProperty('title') ? name = results[i].title : name = results[i].name);
+    if (results[i].hasOwnProperty('title') ? name = results[i].title : name = results[i].name); //Grab name or title
     if (results[i].poster_path != null ? posterImage = imageURL + results[i].poster_path : posterImage = "/assets/phPoster.jpg");
     var posterImage = imageURL + results[i].poster_path;
     var backdropImage = backdropURL + results[i].backdrop_path;
 
-    //Dig Deeper
+    //Expand search results with more details
     var showID = results[i].id;
     var type = results[i].media_type;
-    var deepDetails = "https://api.themoviedb.org/3/"+ type + "/" + showID + "?api_key=" + apiKey + "&language=en-GB";
-    var fetchAllDetails = { "async": true, "crossDomain": true, "url": deepDetails, "method": "GET", "headers": {}, "data": "{}" };
+    var deepDetails = "https://api.themoviedb.org/3/"+ type + "/" + showID + "?api_key=" + apiKey + "&language=en-GB&append_to_response=videos,images,reviews";
 
-    $.ajax(fetchAllDetails).done(function (response) {
-      fullDetails[i] = response;
-      console.log(fullDetails);
+    $.ajax({
+    type: 'GET',
+    url: deepDetails,
+    async: false, //***
+    data: {},
+    dataType: 'json',
+    success: function (data) { fullDetails[i] = data; }
     });
 
+    var genres = fullDetails[i].genres[0].name;
+      if (fullDetails[i].genres.length > 1 ) {
+        for (c = 1; c < fullDetails[i].genres.length; c++) {
+          genres += " | " + fullDetails[i].genres[c].name;
+      }
+    }
 
-
-
+    var runtime = fullDetails[i].runtime;
+    var score = fullDetails[i].vote_average;
+    var imdb = "http://www.imdb.com/title/" + fullDetails[i].imdb_id;
+    var imdbLink = "<a href=\" " + imdb + " \" class=\"imdblink\">View on IMDb</a>";
 
 
 
     //Apply Backdrop Image
     $("<div>", {class: 'result-block', id: curResult}).appendTo(resultsContainer)
-      .css({'background-image': 'radial-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9), rgba(0,0,0,1)), url(' + backdropImage + ')','background-size':'120vw'})
+      .css({'background-image': 'radial-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9), rgba(0,0,0,1)), url(' + backdropImage + ')','background-size':'cover'})
 
     var resultEl = document.getElementById(curResult);
 
@@ -74,11 +86,16 @@ function displayData(data) {
     if (results[i].poster_path != null) { $("query-image"+i).css('background-image', 'url(' + backdropImage + ')') }
     $("<div>", {class: 'query-info', id: 'query-info'+i}).appendTo(resultEl)
       .append("<h1 class=\"box-title\">" + name + "</h1>")
-      .append("<h3 class=\"box-title\">" + name + "</h3>")
+      .append("<h3 class=\"box-genre\">" + genres + "</h3>")
+      .append("<h3 class=\"box-runtime\"id=\"runtime"+i+"\"> Runtime: " + runtime + "m</h3>")
       .append("<h2 class=\"box-summary\">" + results[i].overview + "</h2>")
-      .append("<h4 class=\"box-more\">" + name + "</h4>")
+      .append("<h4 class=\"box-score\"> Viewer Score: " + score + "</h4>")
+      .append("<h4 class=\"box-link\">" + imdbLink + "</h4>")
+      if (type == 'tv') { $("#runtime"+i).remove() }
     }
+    console.log(fullDetails)
   }
+
 
 
 window.onload = function () {
